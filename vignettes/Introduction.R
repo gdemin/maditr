4a7b9ea88d97d23c909971bc15f73bb3f7673624
Gregory Demin <gdemin@gmail.com>
2018-05-09 23:28:38 +0300

    Update readme and vignette

--- vignettes/Introduction.R
+++ vignettes/Introduction.R
@@ -102,3 +102,79 @@ mtcars %>%
         by = list(vsam = vs + am)
     )
 
+## ------------------------------------------------------------------------
+# examples from 'dplyr'
+# newly created variables are available immediately
+mtcars  %>%
+    dt_mutate(
+        cyl2 = cyl * 2,
+        cyl4 = cyl2 * 2
+    ) %>%
+    head()
+
+
+# you can also use dt_mutate() to remove variables and
+# modify existing variables
+mtcars %>%
+    dt_mutate(
+        mpg = NULL,
+        disp = disp * 0.0163871 # convert to litres
+    ) %>%
+    head()
+
+
+# window functions are useful for grouped mutates
+mtcars %>%
+    dt_mutate(
+        rank = rank(-mpg, ties.method = "min"),
+        keyby = cyl) %>%
+    print()
+
+
+# You can drop variables by setting them to NULL
+mtcars %>% dt_mutate(cyl = NULL) %>% head()
+
+# A summary applied without by returns a single row
+mtcars %>%
+    dt_summarise(mean = mean(disp), n = .N)
+
+# Usually, you'll want to group first
+mtcars %>%
+    dt_summarise(mean = mean(disp), n = .N, by = cyl)
+
+
+# Multiple 'by' - variables
+mtcars %>%
+    dt_summarise(cyl_n = .N, by = list(cyl, vs))
+
+# Newly created summaries immediately
+# doesn't overwrite existing variables
+mtcars %>%
+    dt_summarise(disp = mean(disp),
+                  sd = sd(disp),
+                  by = cyl)
+
+# You can group by expressions:
+mtcars %>%
+    dt_summarise_all(mean, by = list(vsam = vs + am))
+
+# filter by condition
+mtcars %>%
+    dt_filter(am==0)
+
+# filter by compound condition
+mtcars %>%
+    dt_filter(am==0,  mpg>mean(mpg))
+
+# grouped filter
+mtcars %>%
+    dt_filter(disp == max(disp), by = cyl)
+
+# select
+mtcars %>% dt_select(vs:carb, cyl)
+mtcars %>% dt_select(-am, -cyl)
+
+# sorting
+dt_arrange(mtcars, cyl, disp)
+dt_arrange(mtcars, -disp)
+
