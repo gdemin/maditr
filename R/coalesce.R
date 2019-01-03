e74d98d53635e02de696ee5a866faaca20e9912a
Gregory Demin <gdemin@gmail.com>
2019-01-03 03:29:20 +0300

    Add 'coalesce' function and prepare for release

---
+++ R/coalesce.R
@@ -0,0 +1,38 @@
+#' Return first non-missing element
+#'
+#' @author Martin Morgan https://stackoverflow.com/users/547331/martin-morgan
+#' @param ... vectors
+#'
+#' @return A vector the same length as the first ... argument with NA
+#'   values replaced by the first non-missing value.
+#' @export
+#'
+#' @examples
+#' # examples from dplyr
+#' x = sample(c(1:5, NA, NA, NA))
+#' coalesce(x, 0L)
+#'
+#' y = c(1, 2, NA, NA, 5)
+#' z = c(NA, NA, 3, 4, 5)
+#' coalesce(y, z)
+coalesce = function(...) {
+    # Martin Morgan https://stackoverflow.com/users/547331/martin-morgan
+    # https://stackoverflow.com/questions/19253820/how-to-implement-coalesce-efficiently-in-r
+    # https://stackoverflow.com/a/19257945/387408
+    args = list(...)
+    ans = args[[1]]
+    length_ans = length(ans)
+    for (elt in args[-1]) {
+
+        if(length(elt)==1){
+            elt = rep_len(elt, length.out = length_ans)
+        } else {
+            if(length(elt)!=length_ans){
+                stop("'coalesce': all arguments should be the same length as first element or length one.", call. = FALSE)
+            }
+        }
+        i = which(is.na(ans))
+        ans[i] = elt[i]
+    }
+    ans
+}
---
+++ tests/testthat/test_coalesce.R
@@ -0,0 +1,12 @@
+context("coalesce")
+
+x = c(1, NA, 2, NA)
+res = coalesce(x, 0L)
+expect_equal( res, c(1, 0, 2, 0))
+expect_error(coalesce(x, 1:2))
+
+y = c(1, 2, NA, NA, 5)
+z = c(NA, NA, 3, 4, 5)
+expect_equal(coalesce(y, z), 1:5)
+
+
