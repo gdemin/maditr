e5d0dc78fa6b70c1208b6a54bd325f1f6679dd3e
Gregory Demin <gdemin@gmail.com>
2019-12-18 19:29:05 +0300

    Add draft 'to_list' and friends. Not enough tests yet and no manual

---
+++ R/to_list.R
@@ -0,0 +1,69 @@
+#' Title
+#'
+#' @param data data.frame/list/vector
+#' @param expr expression
+#' @param skip_null logical Should we skip NULL's from result? Default is TRUE
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' 1
+to_list = function(data, expr = NULL, skip_null = TRUE){
+    expr = substitute(expr)
+    if(is.null(expr)) return(as.list(data))
+
+    expr = substitute_symbols(expr, list(
+        '.item' = quote(data[[.index]]),
+        '.x' = quote(data[[.index]]),
+        '.name' = quote(._names[[.index]]),
+        '.i' = quote(.index)
+    ))
+    ._indexes = seq_along(data)
+    ._names = names(data)
+    names(._indexes) = ._names
+    if(is.null(._names)) ._names = rep("", length(data))
+    fun = eval(substitute_symbols(quote(function(.index) expr), list(expr = expr)))
+    res = lapply(._indexes, fun)
+    if(skip_null){
+        nulls = vapply(res, is.null, FUN.VALUE = logical(1), USE.NAMES = FALSE)
+        res = res[!nulls]
+    }
+    res
+}
+
+#' @rdname to_list
+#' @export
+to_vec = function(data, expr = NULL, skip_null = TRUE, recursive = TRUE, use.names = TRUE){
+     res = eval.parent(substitute(to_list(data, expr, skip_null = skip_null)))
+     unlist(res, recursive = recursive, use.names = use.names)
+
+}
+
+
+#' @rdname to_list
+#' @export
+to_df = function(data, expr = NULL, idcol = NULL){
+    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE)))
+    rbindlist(res, use.names=TRUE, fill=TRUE, idcol = idcol)
+
+}
+
+#' @rdname to_list
+#' @export
+to_dfr = to_df
+
+#' @rdname to_list
+#' @export
+to_dfc = function(data, expr = NULL){
+    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE)))
+    as.data.table(res)
+}
+
+# expr - expression as after 'substitute'
+# symbols - named list  - names will be substituted with values
+substitute_symbols = function(substitute_result, symbols) {
+    eval(bquote(substitute(.(substitute_result), symbols)))
+}
+
+
---
+++ inst/tinytest/test_to_list.R
@@ -0,0 +1,75 @@
+cat("\nContext:","to_list", "\n")
+
+expect_identical(
+    to_list(1:3),
+    as.list(1:3)
+)
+
+data(iris)
+expect_identical(
+    to_list(iris, if(is.numeric(.item)) .item),
+    as.list(iris[1:4])
+)
+
+
+expect_identical(
+    to_list(iris, if(is.numeric(.item)) .item, skip_null = FALSE),
+    c(as.list(iris[1:4]), list(Species = NULL))
+)
+
+
+expect_identical(
+    to_list(iris, if(is.numeric(.x)) .x, skip_null = FALSE),
+    c(as.list(iris[1:4]), list(Species = NULL))
+)
+
+expect_identical(
+    to_list(iris, if(is.numeric(.item)) mean(.item) else uniqueN(.item)),
+    c(as.list(colMeans(iris[1:4])), list(Species = uniqueN(iris$Species)))
+)
+
+expect_identical(
+    to_list(1:5, .i^2 + .index),
+    as.list((1:5)^2 + 1:5)
+
+)
+
+
+
+expect_identical(
+    to_list(iris, if(grepl("Sepal", .name)) .item),
+    as.list(iris)
+
+)
+
+cat("\nContext:","to_vec", "\n")
+expect_identical(
+    to_vec(iris, if(is.numeric(.item)) mean(.item) else uniqueN(.item)),
+    c(colMeans(iris[1:4]), Species = uniqueN(iris$Species))
+)
+
+
+expect_identical(
+    to_vec(iris, if(is.numeric(.item)) mean(.item) else uniqueN(.item), use.names = FALSE),
+    unname(c(colMeans(iris[1:4]), Species = uniqueN(iris$Species)))
+)
+
+
+cat("\nContext:","to_df", "\n")
+
+data("mtcars")
+expect_identical(
+    to_df(mtcars, list(var = .name, mean = mean(.x), sd = sd(.x))),
+    data.table(var = names(mtcars), mean = colMeans(mtcars), sd = sapply(mtcars, sd))
+)
+
+
+expect_identical(
+    to_dfr(mtcars, list(mean = mean(.x), sd = sd(.x)), idcol = "var"),
+    data.table(var = names(mtcars), mean = colMeans(mtcars), sd = sapply(mtcars, sd))
+)
+
+expect_identical(
+    to_dfc(mtcars, c( mean(.x), sd(.x))),
+    as.data.table(rbind(colMeans(mtcars), sd = sapply(mtcars, sd)))
+)
