d7b7a07a21ba879b43fa35194f0383e4f4f87697
Gregory Demin <gdemin@gmail.com>
2019-12-20 17:56:47 +0300

    Draft progress bar in 'to_list'. Not working yet

--- R/to_list.R
+++ R/to_list.R
@@ -9,8 +9,13 @@
 #'
 #' @examples
 #' 1
-to_list = function(data, expr = NULL, skip_null = TRUE){
+to_list = function(data, expr = NULL, skip_null = TRUE,
+                   pb = FALSE,
+                   info = NULL,
+                   pb_step = 1
+                   ){
     expr = substitute(expr)
+    (pb && info) && stop("'to_list': both 'pb' and 'info' are set to TRUE. We can show only one of them - progress bar or info.")
     if(is.null(expr)) return(as.list(data))
 
     expr = substitute_symbols(expr, list(
@@ -23,14 +28,31 @@ to_list = function(data, expr = NULL, skip_null = TRUE){
     ._names = names(data)
     names(._indexes) = ._names
     if(is.null(._names)) ._names = rep("", length(data))
+    ## progress bar and info
+    if(isTRUE(pb)){
+        pbar = txtProgressBar(min = 0, max = length(data), style = 3)
+        ._counter = 0
+        on.exit(close(pbar))
+    }
+    if(!is.null(info)){
+        if(isTRUE(info)){
+            info_expr = quote(cat(Sys.time(), ": ",
+                                  .index, ": ",
+                                  .name, " ",
+                                  if(is.atomic(.item) && length(.item)==1 && object.size(.item)<400) .item,
+                                  "\n", sep = ""))
+        }
+    }
     if(is.symbol(expr) && !identical(expr, quote(.index))){
-        fun = match.fun(eval(expr))
-        res = lapply(data, fun)
+        fun = eval(substitute_symbols(quote(function(.index)
+        {
+            expr(data[[.index]])
+        })), list(expr = expr))
     } else {
         fun = eval(substitute_symbols(quote(function(.index) expr), list(expr = expr)))
-        res = lapply(._indexes, fun)
-    }
 
+    }
+    res = lapply(._indexes, fun)
     if(skip_null){
         nulls = vapply(res, is.null, FUN.VALUE = logical(1), USE.NAMES = FALSE)
         res = res[!nulls]
@@ -40,8 +62,13 @@ to_list = function(data, expr = NULL, skip_null = TRUE){
 
 #' @rdname to_list
 #' @export
-to_vec = function(data, expr = NULL, skip_null = TRUE, recursive = TRUE, use.names = TRUE){
-     res = eval.parent(substitute(to_list(data, expr, skip_null = skip_null)))
+to_vec = function(data, expr = NULL, skip_null = TRUE,
+                  pb = FALSE,
+                  info = NULL,
+                  pb_step = 1,
+                  recursive = TRUE,
+                  use.names = TRUE){
+     res = eval.parent(substitute(to_list(data, expr, skip_null = skip_null, pb = pb, info = info, pb_step = pb_step)))
      unlist(res, recursive = recursive, use.names = use.names)
 
 }
@@ -49,8 +76,12 @@ to_vec = function(data, expr = NULL, skip_null = TRUE, recursive = TRUE, use.nam
 
 #' @rdname to_list
 #' @export
-to_df = function(data, expr = NULL, idcol = NULL){
-    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE)))
+to_df = function(data, expr = NULL,
+                 pb = FALSE,
+                 info = NULL,
+                 pb_step = 1,
+                 idcol = NULL){
+    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE, pb = pb, info = info, pb_step = pb_step)))
     rbindlist(res, use.names=TRUE, fill=TRUE, idcol = idcol)
 
 }
@@ -61,8 +92,11 @@ to_dfr = to_df
 
 #' @rdname to_list
 #' @export
-to_dfc = function(data, expr = NULL){
-    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE)))
+to_dfc = function(data, expr = NULL,
+                  pb = FALSE,
+                  info = NULL,
+                  pb_step = 1){
+    res = eval.parent(substitute(to_list(data, expr, skip_null = TRUE, pb = pb, info = info, pb_step = pb_step)))
     as.data.table(res)
 }
 
