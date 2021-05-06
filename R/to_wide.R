5d069c3c67434188f88e87b1476cc5e50386cc6d
Gregory Demin <gdemin@gmail.com>
2021-05-06 03:16:45 +0300

    Add 'to_wide' and tests for it. No sufficient manual yet

--- R/to_wide.R
+++ R/to_wide.R
@@ -2,13 +2,13 @@
 #'
 #' @param data A data.frame object to convert
 #' @param columns Variables for stacking. When missing, we will stack all columns outside `keep` columns.
-#' @param names_column name of the stacked variable names column. The default name is 'variable'.
-#' @param values_column name of the stacked data values column(s). The default
+#' @param keep columns which will be kept as is, e. g. only recycled or
+#'   deduplicated. If missing, it is all columns except stacked or unstacked.
+#' @param names_in name of the stacked variable names column. The default name is 'variable'.
+#' @param values_in name of the stacked data values column(s). The default
 #'   name is 'value'. Multiple names can be provided here for the case when
 #'   `columns` is a list, though note well that the names provided in
 #'   `columns` take precedence.
-#' @param keep columns which will be kept as is, e. g. only recycled or
-#'   deduplicated. If missing, it is all columns except stacked or unstacked.
 #' @param drop_na If TRUE, NA values will be removed from the stacked data.
 #' @param names_factor If TRUE, the column with names will be converted to
 #'   factor, else it will be a character column. TRUE by default.
@@ -17,7 +17,7 @@
 #' @param fun Function.
 #' @param sep A character.
 #' @param fill = NA,
-#' @param drop A logical. TRUE by default.
+#' @param missing_comb A logical. TRUE by default.
 #' @param ... other arguments passed to `data.table::melt`/`data.table::dcast`
 #' @return data.table in the wide or long form.
 #' @export
@@ -25,10 +25,10 @@
 #' @examples
 #' 1
 to_long = function(data,
-                   columns,
-                   names_column = "variable",
-                   values_column = "value",
-                   keep,
+                   columns = NULL,
+                   keep = NULL,
+                   names_in = "variable",
+                   values_in = "value",
                    drop_na = FALSE,
                    names_factor = TRUE,
                    value_factor = FALSE,
@@ -36,21 +36,20 @@ to_long = function(data,
     parent_frame = parent.frame()
     data_names = names(data)
     columns_expr = substitute(columns)
-    if(!missing(columns_expr)){
-        columns_expr = replace_column_expr(columns_expr, data_names, parent_frame, combine = quote(c))
-        columns = eval(columns_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
-    }
+    columns_expr = replace_column_expr(columns_expr, data_names, parent_frame, combine = quote(c))
+    columns = eval(columns_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
+
     keep_expr = substitute(keep)
-    if(!missing(keep_expr)){
-        keep_expr = replace_column_expr(keep_expr, data_names, parent_frame, combine = quote(c))
-        keep = eval(keep_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
-    }
-    if(missing(columns_expr) && missing(keep_expr)) columns = data_names
+    keep_expr = replace_column_expr(keep_expr, data_names, parent_frame, combine = quote(c))
+    keep = eval(keep_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
+    if(isFALSE(keep)) keep = integer(0)# no keep
+
+    if(is.null(columns) && is.null(keep)) columns = data_names
     maditr::melt(data = data,
                  id.vars = keep,
                  measure.vars = columns,
-                 variable.name = names_column,
-                 value.name = values_column,
+                 variable.name = names_in,
+                 value.name = values_in,
                  na.rm = drop_na,
                  variable.factor = names_factor,
                  value.factor = value_factor,
@@ -62,22 +61,51 @@ to_long = function(data,
 #' @export
 #' @rdname to_long
 to_wide = function(data,
-                   names_column = variable,
-                   values_column = value,
-                   keep,
+                   keep = NULL,
+                   names_in = variable,
+                   values_in = value,
                    fun = identity,
                    sep = "_",
                    fill = NA,
-                   drop = TRUE,
+                   missing_comb = c("none", "rows", "columns", "all"),
                    ...
 ){
+    parent_frame = parent.frame()
+    data_names = names(data)
+    names_in_expr = substitute(names_in)
+    names_in_expr = replace_column_expr(names_in_expr, data_names, parent_frame, combine = quote(c))
+    names_in = eval(names_in_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
+    if(isFALSE(names_in)) names_in = "." # no widening
+
+    values_in_expr = substitute(values_in)
+    values_in_expr = replace_column_expr(values_in_expr, data_names, parent_frame, combine = quote(c))
+    values_in = eval(values_in_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
+
+    keep_expr = substitute(keep)
+    keep_expr = replace_column_expr(keep_expr, data_names, parent_frame, combine = quote(c))
+    keep = eval(keep_expr, setNames(as.list(data_names), data_names), enclos = parent_frame)
+    if(isFALSE(keep)) keep = "."   # no keep
+    if(is.null(keep)) keep = "..." # keep all
+
+
+
+    formula = as.formula(paste(paste(keep, collapse = "+"), "~", paste(names_in, collapse = "+")))
+    missing_comb = match.arg(missing_comb)
+    drop = switch(missing_comb,
+                  "none" = TRUE,
+                  "rows" = c(FALSE, TRUE),
+                  "columns" = c(TRUE, FALSE),
+                  "all"  = c(FALSE, FALSE)
+    )
     maditr::dcast(data = data,
                   formula,
                   fun.aggregate = fun,
                   sep = sep,
                   fill = fill,
                   drop = drop,
-                  value.var = value,
+                  value.var = values_in,
                   ...
     )
 }
+
+
--- inst/tinytest/test_to_wide.R
+++ inst/tinytest/test_to_wide.R
@@ -45,9 +45,10 @@ expect_identical(
 )
 
 expect_identical(
-    to_long(DT, f_1, keep = cols("^i_"), names_column = "var", values_column = "val"),
+    to_long(DT, f_1, keep = cols("^i_"), names_in = "var", values_in = "val"),
     melt(DT, id=1:2, measure="f_1", variable.name = "var", value.name = "val")
 )
+
 
 
 col_var = "^i_"
@@ -79,10 +80,17 @@ expect_identical(
     melt(mtcars, measure.vars = c("am", "vs", "mpg"))
 )
 
+
+data("mtcars")
+expect_identical(
+    to_long(mtcars, columns = c(am, vs, mpg), keep = FALSE),
+    melt(mtcars, measure.vars = c("am", "vs", "mpg"), id = integer(0))
+)
+
 # on na.rm=TRUE. NAs are removed efficiently, from within C
 expect_identical(
-suppressWarnings(to_long(DT, keep = f_1, columns = c(i_1, i_2), drop_na = TRUE)),
-suppressWarnings(melt(DT, id="f_1", measure=c("i_1", "i_2"), na.rm=TRUE)) # remove NA
+    suppressWarnings(to_long(DT, keep = f_1, columns = c(i_1, i_2), drop_na = TRUE)),
+    suppressWarnings(melt(DT, id="f_1", measure=c("i_1", "i_2"), na.rm=TRUE)) # remove NA
 )
 
 expect_identical(
@@ -93,4 +101,131 @@ expect_identical(
 expect_identical(
     to_long(DT, keep=1:2, columns = list(cols("^f_"), cols("^d_")), value_factor=TRUE),
     melt(DT, id=1:2, measure=patterns("^f_", "^d_"), value.factor=TRUE)
+)
+
+
+cat("\nContext:", "to_wide", "\n")
+
+data("ChickWeight")
+names(ChickWeight) = tolower(names(ChickWeight))
+DT = to_long(ChickWeight, keep=2:4) # calls melt.data.table
+
+expect_identical(
+    to_wide(DT, keep = time, fun = mean),
+    dcast(DT, time ~ variable, fun=mean) # using partial matching of argument
+)
+
+expect_identical(
+    to_wide(DT, keep = FALSE, fun = mean),
+    dcast(DT, . ~ variable, fun=mean) # using partial matching of argument
+)
+
+expect_identical(
+    to_wide(DT, keep = diet, fun = mean),
+    dcast(DT, diet ~ variable, fun=mean)
+)
+expect_identical(
+    to_wide(DT, keep = c(diet, chick), names_in = time, missing_comb = "all"),
+    dcast(DT, diet+chick ~ time, drop=FALSE)
+)
+expect_identical(
+    to_wide(DT, keep = c(diet, chick), names_in = time, missing_comb = "all", fill = 0),
+    dcast(DT, diet+chick ~ time, drop=FALSE, fill=0)
+)
+
+expect_equal(
+    to_wide(DT, chick, time, fun = mean),
+    dcast(DT, chick ~ time, fun=mean)
+)
+
+DT <- data.table(v1 = c(1.1, 1.1, 1.1, 2.2, 2.2, 2.2),
+                 v2 = factor(c(1L, 1L, 1L, 3L, 3L, 3L), levels=1:3),
+                 v3 = factor(c(2L, 3L, 5L, 1L, 2L, 6L), levels=1:6),
+                 v4 = c(3L, 2L, 2L, 5L, 4L, 3L))
+# drop=TRUE
+
+expect_error(to_wide(DT, c(v1, v2), v3, missing_comb = "none"))
+expect_identical(
+    to_wide(DT, c(v1, v2), v3, values_in = v4, missing_comb = "none"),
+    dcast(DT, v1 + v2 ~ v3, value.var = "v4")  # default is drop=TRUE
+)
+expect_identical(
+    to_wide(DT, c(v1, v2), v3, values_in = v4, missing_comb = "all"),
+    dcast(DT, v1 + v2 ~ v3, value.var = "v4", drop=FALSE)          # all missing combinations of both LHS and RHS
+)
+expect_identical(
+    to_wide(DT, c(v1, v2), v3, values_in = v4, missing_comb = "rows"),
+    dcast(DT, v1 + v2 ~ v3, value.var = "v4", drop=c(FALSE, TRUE)) # all missing combinations of only LHS
+)
+expect_identical(
+    to_wide(DT, c(v1, v2), v3, values_in = v4, missing_comb = "columns"),
+    dcast(DT, v1 + v2 ~ v3, value.var = "v4", drop=c(TRUE, FALSE)) # all missing combinations of only RHS
+)
+
+# using . and ...
+DT <- data.table(v1 = rep(1:2, each = 6),
+                 v2 = rep(rep(1:3, 2), each = 2),
+                 v3 = rep(1:2, 6),
+                 v4 = rnorm(6))
+expect_identical(
+    to_wide(DT, names_in = v3, values_in = v4),
+    dcast(DT, ... ~ v3, value.var = "v4") #same as v1 + v2 ~ v3, value.var = "v4"
+)
+expect_identical(
+    to_wide(DT, cols(v1 %to% v3), names_in = FALSE, values_in = v4),
+    dcast(DT, v1 + v2 + v3 ~ ., value.var = "v4")
+)
+
+## for each combination of (v1, v2), add up all values of v4
+expect_identical(
+    to_wide(DT, cols("^v(1|2)"), names_in = FALSE, values_in = v4, fun = sum),
+    dcast(DT, v1 + v2 ~ ., value.var = "v4", fun.aggregate = sum)
+)
+
+expect_identical(
+    to_wide(DT, cols("v{1:2}"), names_in = FALSE, values_in = v4, fun = sum),
+    dcast(DT, v1 + v2 ~ ., value.var = "v4", fun.aggregate = sum)
+)
+
+my_keep = "v{1:2}"
+my_names = FALSE
+my_values = "v4"
+
+expect_error(to_wide(DT, my_keep, names_in = FALSE, values_in = v4, fun = sum))
+
+
+expect_identical(
+    to_wide(DT, cols(my_keep), names_in = my_names, values_in = my_values, fun = sum),
+    dcast(DT, v1 + v2 ~ ., value.var = "v4", fun.aggregate = sum)
+)
+
+# multiple value.var and multiple fun.aggregate
+DT = data.table(x=sample(5,20,TRUE), y=sample(2,20,TRUE),
+                z=sample(letters[1:2], 20,TRUE), d1 = runif(20), d2=1L)
+# multiple value.var
+expect_identical(
+    to_wide(DT, c(x, y), z, c(d1, d2), fun = sum, fill = 0),
+    dcast(DT, x + y ~ z, fun=sum, value.var=c("d1","d2"))
+)
+# multiple fun.aggregate
+expect_identical(
+    to_wide(DT, c(x, y), z, d1, fun = list(sum = sum, mean = mean), fill = NULL),
+    dcast(DT, x + y ~ z, fun=list(sum, mean), value.var="d1")
+)
+
+
+my_fun = list(sum = sum, mean = mean)
+expect_identical(
+    to_wide(DT, c(x, y), z, d1, fun = my_fun),
+    dcast(DT, x + y ~ z, fun=my_fun, value.var="d1", fill = NA)
+)
+# multiple fun.agg and value.var (all combinations)
+expect_identical(
+    to_wide(DT, c(x, y), z, c(d1, d2), fun = my_fun),
+    dcast(DT, x + y ~ z, fun=my_fun, value.var=c("d1", "d2"), fill = NA)
+)
+# multiple fun.agg and value.var (one-to-one)
+expect_identical(
+    to_wide(DT, c(x, y), z, list(d1, d2), fun = my_fun),
+    dcast(DT, x + y ~ z, fun=my_fun, value.var=list("d1", "d2"), fill = NA)
 )
